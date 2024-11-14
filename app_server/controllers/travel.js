238b19b26a8fafcb1ee069f7f4736b1200b9b007
kendall white <kendallw763@gmail.com>
2024-11-14 09:51:09 -0500

    Module3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,8 @@
 //get homepage
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','uft8'));
+
+
 const travel =(req, res) => {
     res.render('travel' , {title: "Travlr Getaways"})
 };
